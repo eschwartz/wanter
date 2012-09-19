f5f67fb1a1358ee76cf8738ac1a461bc6bf0d99f
Edan Schwartz <eschwartz@mindboxstudios.com>
2012-09-19 14:17:22 -0400

    Initialized layout of ProductsApp

--- modular.php
+++ modular.php
@@ -3,12 +3,30 @@
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 <title>Wanter - Modular</title>
+<script type="text/javascript" src="../lib/jquery.js"></script>
+<script type="text/javascript" src="../lib/jquery.masonry.min.js"></script>
+<script type="text/javascript" src="../lib/underscore.js"></script>
+<script type="text/javascript" src="../lib/backbone.js"></script>
+<script type="text/javascript" src="../lib/backbone.paginator.js"></script>
+<script type="text/javascript" src="../lib/backbone.marionette.js"></script>
+
 </head>
+
+<body>
+
+<h2>Following the <a href="http://davidsulc.com/blog/2012/05/06/tutorial-a-full-backbone-marionette-application-part-1/" target="_blank">Marionette App Tutorial</a></h2>
+
+
+<div id="products"></div>
+
+
 
 <!-- TEMPLATES
  ================== -->
-
-<div id="products"></div>
+<script id="products-layout" type="text/html">
+	<div id="search"></div>
+	<div id="product-list"></div>
+</script>
 
 <!-- END TEMPLATES
  ================== -->
@@ -21,9 +39,11 @@
 var WanterApp = new Backbone.Marionette.Application();
 
 // Set App Regions
-Wanter.addRegions({
+WanterApp.addRegions({
 	products: '#products'
 });
+
+WanterApp.start();
 </script>
 
 
@@ -35,15 +55,40 @@ Wanter.addRegions({
 WanterApp.ProductsApp = function() {
 	var ProductsApp = {};
 	
+	var Layout = Backbone.Marionette.Layout.extend({
+		template: '#products-layout',
+		regions: {
+			search: '#search',
+			products: '#product-list'
+		}
+	});
+	
+	ProductsApp.initializeLayout = function() {
+		// Instantiate Layout
+		ProductsApp.layout = new Layout();
+		
+		// Trigger rendered event on show
+		ProductsApp.layout.on("show", function() {
+			WanterApp.vent.trigger("layout:rendered");
+		});
+		
+		// Show layout in WanterApp's products region
+		WanterApp.products.show(ProductsApp.layout);
+	}
+	
+	
 	return ProductsApp;
 }();
+
+
+// Initialize ProductsApp layout when the parent app loads
+WanterApp.addInitializer(function() {
+	WanterApp.ProductsApp.initializeLayout();
+});
 
 </script>
 
 
-<body>
-
-<h2>Following the <a href="http://davidsulc.com/blog/2012/05/06/tutorial-a-full-backbone-marionette-application-part-1/" target="_blank">Marionette App Tutorial</a></h2>
 
 
 
