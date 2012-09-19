d6b09d4a1ced5dade793c631032a31626bc138ce
Edan Schwartz <eschwartz@mindboxstudios.com>
2012-09-19 14:04:45 -0400

    Mostly Styling

--- index.php
+++ index.php
@@ -9,11 +9,36 @@ body {
 	background: #f1f1f1;
 }
 
+p {
+	font-family:ff-meta-web-pro, "Lucida Grande", sans-serif;
+	font-size:13px;
+	line-height:1.5em;
+}
+
+a {
+  color: #000;
+  margin-top: 16px;
+  font-family: proxima-nova;
+  letter-spacing: 0.2em;
+  font-size: 12px;
+  font-weight: bold;
+  border-bottom: 2px solid #000;
+  text-transform: uppercase;
+	text-decoration: none;
+}
+
 .container {
 	background:#fff;
 	border:1px solid #ccc;
 	width:960px;
 	margin:0 auto;
+}
+
+.left {
+	float:left;
+}
+.right {
+	float:right;
 }
 
 .clear {
@@ -24,24 +49,136 @@ body {
 	margin:0;
 }
 	.productList .item {
+		position:relative;
 		float:left;
 		margin:20px;
 		padding:10px;
 		cursor:pointer;
+	}
+	/* arrow on active item */
+	.item.active:before {
+	  content: '';
+	  width: 0em;
+	  height: 0em;
+	  border-left: 14px solid transparent;
+	  border-right: 14px solid transparent;
+	  border-bottom: 14px solid #ccc;
+	  position: absolute;
+	  bottom: -16px;
+	  left: 50%;
+	  margin-left: -14px;
+	  z-index:99;
+	}
+	.item.active:after {
+		  content: '';
+		  width: 0em;
+		  height: 0em;
+		  border-left: 14px solid transparent;
+		  border-right: 14px solid transparent;
+		  border-bottom: 14px solid #fff;
+		  position: absolute;
+		  bottom: -18px;
+		  left: 50%;
+		  margin-left: -14px;
+		  z-index: 100;
 	}
 		.productList .item img {
 			height:170px;
 			width:170px;
 		}
 
-.productDetails {
-	list-style:none;
-	padding:15px;
-	border:1px solid rgba(0,0,0,.7);
+.item.productDetails {
+	width:100%;
+	box-sizing:border-box;
+	padding:30px;
+	margin: -5px 0 20px 0;
+	border-top:1px solid #ccc;
+	border-bottom:1px solid #ccc;
 }
+	.item.productDetails .image img {
+		width:400px;
+		height:auto;
+	}
+	.item.productDetails .content {
+		width: 300px;
+		margin-left: 100px;
+		padding: 0 20px;
+	}
+	.item h2 {
+		font-family: ff-meta-web-pro, "Lucida Grande", sans-serif;
+		font-size:16px;
+		line-height:1.33em;
+	}
+	.item h5 {
+		font-family: ff-meta-web-pro, "Lucida Grande", sans-serif;
+		color:#8d8d8d;
+		font-size:14px;
+		padding-bottom:20px;
+		margin:0;
+	}
+	.item .btn {
+		width:180px;
+	}
+	
+	
+	
+	
+.btn {
+	padding:10px 0;
+
+  display:inline-block;
+
+/*  font-family: "Proxima Nova", "Helvetica", "Arial", sans-serif; */
+  font-family: proxima-nova, "Helvetica", "Arial", sans-serif;
+  font-size:13px;
+  font-weight: bold;
+  letter-spacing:0.25em;
+  text-transform:uppercase;
+  text-decoration:none;
+  text-align:center;
+  -webkit-font-smoothing: antialiased;
+  text-rendering: optimizeLegibility;
+
+
+  border:none;
+  -webkit-appearance: none;
+
+  box-sizing:border-box;
+  -webkit-border-radius: 0.25em;
+  -moz-border-radius: 0.25em;
+  border-radius: 0.25em;
+
+  border:0;
+
+  background-color: #000000;
+  color: #FFFFFF;
+
+}
+
+.btn:hover, .btn:visited {
+  color: #fff;
+}
+
+.btn.default:active {
+  color: #DDDDDD;
+}
+
+.btn:active {
+  background-color: #333;
+  color: #fff;
+}
+
+.btn.action {
+  height:4.233em;
+  line-height: 4.233em;
+
+  box-shadow:0 0.333em 0 #000;
+  filter:progid:DXImageTransform.Microsoft.dropshadow(OffX=0, OffY=3, Color='000', Positive='true');
+}	
 </style>
 
 <title>Wanter - Backbone Clone</title>
+
 <script type="text/javascript" src="../lib/jquery.js"></script>
 <script type="text/javascript" src="../lib/jquery.masonry.min.js"></script>
 <script type="text/javascript" src="../lib/underscore.js"></script>
@@ -172,10 +309,18 @@ _.extend(Backbone.Marionette.View.prototype, {
 </script>
 
 <script id="product-details-template" type="text/html">
-	<div>
-		<h2>name: descr</h2>
-		<button class="close">Close</button>
+	<div class="left image">
+		<img src="<%=imageSrc() %>" alt="<%=title %>" />
 	</div>
+	<div class="left content">
+		<h2><%=title %></h2>
+        <h5><%=brand %></h5>
+        <button class="btn">Add to Gift</button>
+        <p><%=description %></p>
+        <p><a href="#">Learn more</a></p>
+	</div>   
+	<div class="clear"></div>
+
 </script>
 
 <script type="text/javascript">
@@ -269,6 +414,12 @@ Wanter.ProductDetailsView = Backbone.Marionette.ItemView.extend({
 	className: 'productDetails item',
 	template: '#product-details-template',
 	
+	templateHelpers: {
+		imageSrc: function() {
+			return this.images[0].link;
+		}
+	},
+	
 	ui: {
 		closeBtn: '.close'
 	},
@@ -278,10 +429,15 @@ Wanter.ProductDetailsView = Backbone.Marionette.ItemView.extend({
 	},
 	
 	onRender: function() {
-		this.$el.hide().fadeIn();
+		var self = this;
+		
+		this.$el.hide();
+		window.setTimeout(function() {				// This is crappy hack, because the CollectionView is inserting the $el after render is called.
+			self.$el.slideDown("slow");
+		}, 15);
 	},
 	beforeClose: function(close) {
-		this.$el.fadeOut(close);
+		this.$el.slideUp(close);
 		return false;
 	}
 });
@@ -296,7 +452,7 @@ Wanter.ProductView = Backbone.Marionette.ItemView.extend({
 	
 	templateHelpers: {
 		thumbSrc: function() {
-			return this.images[0].link
+			return this.images[0].link;
 		}
 	},
 	
@@ -343,10 +499,10 @@ Wanter.ProductListView = Backbone.Marionette.CollectionView.extend({
 	perRow: 4,							// Items per row. Used to add detailView after last item in row
 
 	initialize: function() {
-		// Bind view to products colletions
-		this.collection.on("reset", this.render, this);
+		console.log(this.collection)
 		
 		// Handle opening and closing of detail views
+		// NOTE: all this should be moved to a controller
 		Wanter.vent.bind('details:request', this.openProductDetails, this);
 	},
 	
@@ -362,19 +518,30 @@ Wanter.ProductListView = Backbone.Marionette.CollectionView.extend({
 			$lastItemInRow = this.$el.find('.item:eq('+lastItemIndex +')'),
 			detailView = new this.detailView({ model: itemModel}),
 			self = this;
-			
+		
+		// Insert the detail view at the end of the row	
 		var insertDetailView = function() {
+			// Reset "active" class
+			self.$el.find('.item').removeClass('active');
+			itemView.$el.addClass('active');
+			
 			detailView.render().$el.insertAfter($lastItemInRow);
 			self.openDetailView = detailView;
 		}
 		
+		// If same view, exit
+		if(this.openDetailView && this.openDetailView.model === detailView.model) {
+			return false;
+		}
+		
+		// If no detail view, show this one
 		if(!this.openDetailView) {
 			insertDetailView();
 			return;
 		}
 		
 		// Close existing view, then open new details view.
-		this.openDetailView.close(insertDetailView());
+		this.openDetailView.close(insertDetailView);
 	}
 });
 
