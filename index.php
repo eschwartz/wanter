0c1ee425de0ac607023404a8281f79f95b129f7a
Edan Schwartz <eschwartz@mindboxstudios.com>
2013-01-15 21:29:23 -0500

    Restructured files. Moved vendor libraries into repo.

--- index.php
+++ index.php
@@ -1,37 +1,64 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml"><head>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title>Wanter - Modular</title>
 
 <link href="wanter.css" type="text/css" rel="stylesheet" />
 
-<title>Wanter - Backbone Clone</title>
+<script type="text/javascript" src="lib/jquery.js"></script>
+<script type="text/javascript" src="lib/jquery.masonry.min.js"></script>
+<script type="text/javascript" src="lib/jquery.scrollTo.js"></script>
 
-<script type="text/javascript" src="../lib/jquery.js"></script>
-<script type="text/javascript" src="../lib/jquery.masonry.min.js"></script>
-<script type="text/javascript" src="../lib/underscore.js"></script>
-<script type="text/javascript" src="../lib/backbone.js"></script>
-<script type="text/javascript" src="../lib/backbone.paginator.js"></script>
-<script type="text/javascript" src="../lib/backbone.marionette.js"></script>
+<script type="text/javascript" src="lib/underscore.js"></script>
+<script type="text/javascript" src="lib/backbone.js"></script>
+<script type="text/javascript" src="lib/backbone.paginator.js"></script>
+<script type="text/javascript" src="lib/backbone.marionette.js"></script>
 <script type="text/javascript" src="Backbone.Marionette.extended.js"></script>
+
 </head>
 
 <body>
 
-<div id="profile"></div>
 
-<div id="products" class='container'>
+<div class="container">
+	<div id="products-region" class="container"></div>
+    <div class="clear"></div>
 </div>
-<div class="clear"></div>
 
-<div id="cart"></div>
+<div id="flash-container">
+</div>
 
-<script id="loading-template" type="text/html">
-	<div class="loading">Loading...</div>
+
+
+<!-- TEMPLATES
+ ================== -->
+ 
+
+<script id="products-layout" type="text/html">
+	<div id="search">
+		<input type="text" class="inline-block" placeholder="Search the Google Shopping API" name="term" id="term" />
+	</div>
+	<div id="product-list" class="left"></div>
+	<div id="sidebar" class="left">
+		<div id="cart"></div>
+	</div>
 </script>
 
+<script id="product-list-template" type="text/html">
+	<div class="productList">
+		<div id="clearList" class="clear"></div>
+	</div>
+</script>
 
 <script id="product-template" type="text/html">
 	<img src="<%=thumbSrc() %>" alt="<%=title %>" />
+	<div class="<%=inCart? 'cross': 'check'%> toggleCart"></div>
+</script>
+
+<script id="detail-container-template" type="text/html">
+	<div class="details">
+	</div>
 </script>
 
 <script id="product-details-template" type="text/html">
@@ -40,238 +67,66 @@
 	</div>
 	<div class="left content">
 		<h2><%=title %></h2>
-        <h5><%=brand %></h5>
-        <button class="btn">Add to Gift</button>
-        <p><%=description %></p>
-        <p><a href="#">Learn more</a></p>
+		<h5><%=brand %></h5>
+		
+		<button class="btn toggleCart">
+			<%=inCart? "Remove from Cart": "Add to Cart" %>
+		</button>
+		
+		<p><%=description %></p>
+		<p><a href="<%=link %>" target="_blank">Learn more</a></p>
 	</div>   
 	<div class="clear"></div>
 </script>
 
+
+<!-- Cart Templates
+ ===================== -->
+<!-- Cart Item  -->
+<script id="cart-item-template" type="text/html" >
+	<a href="#"><img src="<%=thumbSrc() %>" alt="<%=title %>" /></a>
+</script>
+
+<!-- Empty cart -->
+<script id="cart-empty-template" type="text/html" >
+	<div id="cartList_empty">
+		<img src="add.png" />
+		<p>Add</p>
+		<p style="font-weight:bold; font-size:20px;">16</p>
+		<p>products</p>
+	</div>	
+</script>
+
+
+ 
+
+<!-- Flash Message Template
+ =========================== -->
+<script id="flash-template" type="text/html">
+	<%=value %>
+</script>
+
+
+<!-- END TEMPLATES
+ ================== -->
+
+
+<script type="text/javascript" src="app/WanterApp.js"></script>
+<script type="text/javascript" src="app/WanterApp.Flash.js"></script>
+<script type="text/javascript" src="app/WanterApp.ProductsApp.products.js"></script>
+<script type="text/javascript" src="app/WanterApp.ProductsApp.js"></script>
+<script type="text/javascript" src="app/WanterApp.ProductsApp.Router.js"></script>
+<script type="text/javascript" src="app/WanterApp.ProductsApp.ProductList.js"></script>
+<script type="text/javascript" src="app/WanterApp.ProductsApp.ProductSearch.js"></script>
+<script type="text/javascript" src="app/WanterApp.ProductsApp.ShoppingCart.js"></script>
 <script type="text/javascript">
-
-
-// TO DO NEXT: Try creating a relation to a images model, using Backbone relations extension
-var Product = Backbone.Model.extend({
-	idAttribute: "googleId",
-	parse: function(response) {
-		return response.product;
-	}
-});
-
-
-var ProductCollection = Backbone.Paginator.requestPager.extend({
-	model: Product,
-	
-	paginator_core: {
-		url: 'https://www.googleapis.com/shopping/search/v1/public/products?'	
-	},
-	paginator_ui: {
-		firstPage	: 0,
-		currentPage	: 0,
-		perPage		: 20,
-		totalPages	: 10			// a default, in case we fail to calculate the total pages
-	},
-	server_api: {
-		'key'			: 'AIzaSyDx-MNjeT9vIXdcKBvVaXZEOeVPRju8fBE',
-		'country'		: 'US',
-		'q'				: "business casual blazer men's",
-		'startIndex'	: function() { return (this.currentPage * this.perPage) + 1 },		// Google uses 1 based index
-		'maxResults'	: function() { return this.perPage }
-		
-	},
-	parse: function(response) {
-		// Example JSON: 	
-		// https://www.googleapis.com/shopping/search/v1/public/products?key=AIzaSyDx-MNjeT9vIXdcKBvVaXZEOeVPRju8fBE&country=US&language=en&currency=USD
-		var tags = response.items;
-		
-		this.totalPages = Math.ceil(response.totalItems / this.perPage);
-		
-		return tags;
-	}
+// bootstrap 
+// Important: this needs to be the very last thing, or everything's messed up with order of operations
+$(document).ready(function() {
+	WanterApp.start();
 });
 </script>
 
-<script type="text/javascript">
-
-var Wanter = new Backbone.Marionette.Application();
-
-/**
- * Initialize Application
-*/
-Wanter.addInitializer(function(options) {
-	var productCollection = new ProductCollection();
-	var productList = new this.ProductListView({
-		collection: productCollection
-	});
-	
-	// Fetch first page of products collection
-	productCollection.goTo(0);
-	
-	// Show the productList view in the products region
-	this.products.show(productList);
-	
-});
-
-/**
- * Application regions
-*/
-Wanter.addRegions({
-	profile: '#profile',
-	products: '#products',
-	cart: '#cart'
-});
-
-
-/**
- * All purpose loading view
-*/
-Wanter.LoadingView = Backbone.Marionette.ItemView.extend({
-	template: '#loading-template',
-});
-
-
-/** 
- * ProductDetailsView
- * more info about a product
-*/
-Wanter.ProductDetailsView = Backbone.Marionette.ItemView.extend({
-	className: 'productDetails item',
-	template: '#product-details-template',
-	
-	templateHelpers: {
-		imageSrc: function() {
-			return this.images[0].link;
-		}
-	},
-	
-	ui: {
-		closeBtn: '.close'
-	},
-	
-	events: {
-		'click closeBtn' : 'close'
-	},
-	
-	onRender: function() {
-		var self = this;
-		
-		this.$el.hide();
-		window.setTimeout(function() {				// This is crappy hack, because the CollectionView is inserting the $el after render is called.
-			self.$el.slideDown("slow");
-		}, 15);
-	},
-	beforeClose: function(close) {
-		this.$el.slideUp(close);
-		return false;
-	}
-});
-
-/**
- * ProductView
- * a sinlge product ItemView
-*/
-Wanter.ProductView = Backbone.Marionette.ItemView.extend({
-	template: '#product-template',
-	className: 'item',
-	
-	templateHelpers: {
-		thumbSrc: function() {
-			return this.images[0].link;
-		}
-	},
-	
-	initialize: function() {
-		_.bindAll(this,'handleRequestDetails', 'closeDetails'); 
-	},
-	
-	events: {
-		'click' : 'handleRequestDetails'
-	},
-	
-	onRender: function() {
-		var self = this;
-		
-		self.$el.fadeTo(0,0, function() {
-			self.$el.imagesLoaded(function() {
-				self.$el.delay(100).fadeTo(800, 1);
-			});
-		});
-	},
-	
-	// Broadcast the details request
-	handleRequestDetails: function() {
-		// I'll let the list view handle opening and closing details
-		Wanter.vent.trigger('details:request', this, this.model);
-	},
-	
-	closeDetails: function() {
-		this.openDetailView.close();
-	},
-});
-
-
-/**
- * ProductsListView
- * List view of all products
-*/
-Wanter.ProductListView = Backbone.Marionette.CollectionView.extend({
-	className: 'productList',
-	itemView: Wanter.ProductView,
-	openDetailView: null,
-	detailView: Wanter.ProductDetailsView,
-	emptyView: Wanter.LoadingView,
-	perRow: 4,							// Items per row. Used to add detailView after last item in row
-
-	initialize: function() {
-		console.log(this.collection)
-		
-		// Handle opening and closing of detail views
-		// NOTE: all this should be moved to a controller
-		Wanter.vent.bind('details:request', this.openProductDetails, this);
-	},
-	
-	onRender: function() {
-		this.$el.append($('<div class="clear"></div>'));			// Kind of a crappy way to do this... but jquery overwrites my inline block
-	},
-	
-	// Handle switching between detail views
-	openProductDetails: function(itemView, itemModel) {
-		// Find the last item in this row
-		var row = Math.ceil(itemView.$el.index() / this.perRow),
-			lastItemIndex = parseInt(row * this.perRow - 1),
-			$lastItemInRow = this.$el.find('.item:eq('+lastItemIndex +')'),
-			detailView = new this.detailView({ model: itemModel}),
-			self = this;
-		
-		// Insert the detail view at the end of the row	
-		var insertDetailView = function() {
-			// Reset "active" class
-			self.$el.find('.item').removeClass('active');
-			itemView.$el.addClass('active');
-			
-			detailView.render().$el.insertAfter($lastItemInRow);
-			self.openDetailView = detailView;
-		}
-		
-		// If same view, exit
-		if(this.openDetailView && this.openDetailView.model === detailView.model) {
-			return false;
-		}
-		
-		// If no detail view, show this one
-		if(!this.openDetailView) {
-			insertDetailView();
-			return;
-		}
-		
-		// Close existing view, then open new details view.
-		this.openDetailView.close(insertDetailView);
-	}
-});
-
-Wanter.start();
-</script>
-
+<div id="flash-area"></div>
 </body>
 </html>
